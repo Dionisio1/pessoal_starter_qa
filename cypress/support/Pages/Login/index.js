c459940d9cb5700cbdcd7db8cf01308d5329e87a
Dionisio Soares <dio.soares@gmail.com>
2023-05-07 23:49:23 -0300

    adicionado fexture formularioLogin

--- cypress/support/Pages/Login/index.js
+++ cypress/support/Pages/Login/index.js
@@ -1,6 +1,7 @@
 /// <reference types="Cypress"/>
-import dataLogin from "../../../fixtures/dataLogin.json"
-import mensages  from "../../../fixtures/mensages.json"
+import dataLogin        from "../../../fixtures/dataLogin.json"
+import mensages         from "../../../fixtures/mensages.json"
+import formularioLogin  from "../../../fixtures/formularioLogin.json"
 
 const elLogin = require('./elements').ELEMENTS
 
@@ -26,7 +27,7 @@ class Login {
     };
 
     signupNameEmail() {
-        cy.get(elLogin.fieldSignupName).click().type('Dio Teste');
+        cy.get(elLogin.fieldSignupName).click().type(formularioLogin.nameSignup);
         cy.get(elLogin.fieldSignupEmail).click().type(dataLogin.correctEmail);
     };
 
@@ -43,17 +44,17 @@ class Login {
     };
 
     FormSignupName() {
-        cy.get(elLogin.fieldFormNameSignup).click().clear().type('Dionisio Teste');    
+        cy.get(elLogin.fieldFormNameSignup).click().clear().type(formularioLogin.informationName);    
     };
 
     FormSignupPassword() {
-        cy.get(elLogin.fieldFormPasswordSignup).click().type('123456');
+        cy.get(elLogin.fieldFormPasswordSignup).click().type(formularioLogin.infomationPassword);
     };
 
     FormSignupDateOfBirth() {
-        cy.get(elLogin.fieldFormDayBirthSignup).select('6');
-        cy.get(elLogin.fieldFormMonthBirthSignup).select('September');
-        cy.get(elLogin.fieldFormYearBirthSignup).select('1982')
+        cy.get(elLogin.fieldFormDayBirthSignup).select(formularioLogin.informationDay);
+        cy.get(elLogin.fieldFormMonthBirthSignup).select(formularioLogin.informationMonth);
+        cy.get(elLogin.fieldFormYearBirthSignup).select(formularioLogin.informationYear);
     };
 
     FormSignupNewsletterOtions() {
@@ -62,24 +63,24 @@ class Login {
     };
 
     FormSignupAddress() {
-        cy.get(elLogin.fieldFormFirstName).click().type('Dionisio');
-        cy.get(elLogin.fieldFormLastName).click().type('Teste Automação');
-        cy.get(elLogin.fieldFormCompany).click().type('Minha Automacao');
-        cy.get(elLogin.fieldFormAddress).click().type('Rua da automacao, 22 - Bobotization');
-        cy.get(elLogin.fieldFormAddress2).click().type('Campo de endereço para segunda informação');
-        cy.get(elLogin.fieldFormCountry).select('United States');
-        cy.get(elLogin.fieldFormState).click().type('Ohio')
-        cy.get(elLogin.fieldFormCity).click().type('Oclaroma');
-        cy.get(elLogin.fieldFormZipcode).click().type('58000000');
-        cy.get(elLogin.fieldFormMobileNumber).click().type('83981255238');
+        cy.get(elLogin.fieldFormFirstName).click().type(formularioLogin.adressName);
+        cy.get(elLogin.fieldFormLastName).click().type(formularioLogin.adressLastName);
+        cy.get(elLogin.fieldFormCompany).click().type(formularioLogin.adressCompany);
+        cy.get(elLogin.fieldFormAddress).click().type(formularioLogin.adress);
+        cy.get(elLogin.fieldFormAddress2).click().type(formularioLogin.adress2);
+        cy.get(elLogin.fieldFormCountry).select(formularioLogin.adressCountry);
+        cy.get(elLogin.fieldFormState).click().type(formularioLogin.adressState);
+        cy.get(elLogin.fieldFormCity).click().type(formularioLogin.adressCity);
+        cy.get(elLogin.fieldFormZipcode).click().type(formularioLogin.adressZipcode);
+        cy.get(elLogin.fieldFormMobileNumber).click().type(formularioLogin.adressPhone);
     };
 
     createAccount() {
         cy.get(elLogin.btnCreateAccount).click();
-        cy.get(elLogin.msgActionAccount).should('have.text', 'Account Created!');
+        cy.get(elLogin.msgActionAccount).should('have.text', mensages.MsgAccountCreated);
         cy.get(elLogin.btnContinue).click();    
     };
-
+    
     isUserLogged() {
         cy.get(elLogin.btnLogout).should('have.text', ' Logout');
         cy.get(elLogin.avatarLogged).should('be.visible');
@@ -101,10 +102,7 @@ class Login {
     }
 
     validateMsgExistingAccount(){
-        cy.wait(2000);
-        // cy.get('.signup-form > form > p').should('be.visible')
         cy.get(elLogin.fieldMsgExistEmail).should('be.visible');
-        // cy.get('.signup-form > form > p').should('have.text', mensages.failMsgCreateAccount)
         cy.get(elLogin.fieldMsgExistEmail).should('have.text', mensages.failMsgCreateAccount)
     }
 };
