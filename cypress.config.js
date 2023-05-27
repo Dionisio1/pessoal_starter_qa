897f8710733cd182a1821dcd05507c0c9dd7985b
Dionisio Soares <dio.soares@gmail.com>
2023-05-27 13:07:14 -0300

    add Mochawesome report

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,15 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require("cypress");                          //Mochawesome report
 const cucumber = require('cypress-cucumber-preprocessor').default;
+const allureWriter = require('@shelex/cypress-allure-plugin/writer'); //report allure
 
 module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',                           //Mochawesome report
   e2e: {
     setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on);             //Mochawesome report
       on('file:preprocessor', cucumber())
+      allureWriter(on, config);                                       //report allure
+      return config;                                                  //report allure
     },
     specPattern: "cypress/e2e/**/*.{js,jsx,ts,tsx,feature}"
   },
